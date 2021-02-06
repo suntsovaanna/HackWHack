02fd8791824e53e0073151d6c30e7723caf6fcdd
Анна Максимова <ann.maksimova@elewise.com>
2021-02-06 10:51:17 +0300

    Mocked Naming API with placeholders

--- server.py
+++ server.py
@@ -1,13 +1,39 @@
 import uvicorn
 from fastapi import FastAPI
-
+from typing import List
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class NameQuery(BaseModel):
+    fullName: str
 
-@app.get('/help')
-async def help():
-    return 'Hello'
+class BulkNameQuery(BaseModel):
+    fullNames: List[str]
+
+@app.post('/name')
+async def parsename(data: NameQuery):
+    return data.fullName
+
+
+@app.post('/names')
+async def parsenamearray(data: BulkNameQuery):
+    return data.fullNames[0]
+
+
+@app.get('/settings')
+async def getSettings():
+    return "there is no settings yet"
+
+
+@app.post('/settings')
+async def updateSettings():
+    return ""
+
+
+@app.delete('/settings')
+async def resetSettings():
+    return
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=9000)  # noqa
