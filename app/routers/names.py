1659d8bb240e98dfef142ec225d59602f315ce92
Анна Максимова <ann.maksimova@elewise.com>
2021-02-06 19:23:06 +0300

    Added Spellchecker package and functionality

--- app/routers/names.py
+++ app/routers/names.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 from functools import lru_cache
 from recognizer.recognition import NameRecognizer
 from recognizer.grammars import name_parser
-
+from spellchecker import SpellChecker
 
 router = APIRouter()
 
@@ -20,10 +20,31 @@ def get_named_parser():
 
 NAME_RECOGNIZER = Depends(get_named_parser)
 
+spell = SpellChecker(language=None, case_sensitive=False)
+spell.word_frequency.load_text_file('../data/first.txt')
+spell.word_frequency.load_text_file('../data/last.txt')
+
+
+def correct(part: str):
+    if part in spell:
+        return part
+    else:
+        return correction(part)
+
+
+@router.get('/check')
+def correction(name: str):
+    is_lower = name.islower()
+    result = spell.correction(name)
+    return result if is_lower else result.capitalize()
+
 
 @router.post('/name')
 def parse_name(data: NameQuery, name_recognizer: NameRecognizer = NAME_RECOGNIZER):
-    return name_recognizer(data.fullName)
+    parts = data.fullName.split(sep=' ')
+    lower_parts = [correct(part) for part in parts]
+    joined = ' '.join(lower_parts)
+    return joined, name_recognizer(joined)
 
 
 @router.post('/names')
